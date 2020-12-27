3dd27a45324b3051cf45d4d62ea1a14467373b3d
lmunck <anders.munck@gmail.com>
2020-12-27 10:15:21 +0100

    Initial Commit

---
+++ DragDropSortFilterDelete/DragDropSortFilterDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropSortFilterDeleteApp.swift
+//  DragDropSortFilterDelete
+//
+//  Created by Anders Munck on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct DragDropSortFilterDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
