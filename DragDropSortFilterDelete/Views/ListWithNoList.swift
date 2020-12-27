632417427d2a2411ae479bc18486233fe97f28c0
lmunck <anders.munck@gmail.com>
2020-12-27 18:25:16 +0100

    ff

--- DragDropSortFilterDelete/Views/ListWithNoList.swift
+++ DragDropSortFilterDelete/Views/ListWithNoList.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+/*
+ 
+WHAT I WOULD LIKE:
+ 
+Get the same functionality as when rearranging icons on iOS homescreen without using List:
+
+When long-pressing a card, you enter editmode. When in editmode, the cards wiggle, and a xmark is shown in top right corner to delete it.
+ 
+When dragging a card horizontally, the other cards move away to make room, and the scroll follows along.
+ 
+ When dropping a card, the card is moved in the array to the new position and the view is updated.
+ 
+*/
+
+
 struct ListWithNoList: View {
     
     @State private var editModeActive:Bool = true
@@ -15,12 +30,13 @@ struct ListWithNoList: View {
     var body: some View {
         VStack {
             
-            // Edit button
+            // Editmode button
             Button(action: { editModeActive.toggle() }, label: {
-                Text(editModeActive ? "Active" : "Inactive").foregroundColor(.white)
+                Text(editModeActive ? "Editmode Active" : "Editmode inactive").foregroundColor(.white)
             })
             
             // List of cards
+            
             ScrollView(.horizontal) {
                 LazyHStack {
                     ForEach (Contact.testArray) { contact in
